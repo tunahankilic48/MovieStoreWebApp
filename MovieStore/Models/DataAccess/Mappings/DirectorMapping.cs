c68a6f509882a2e5810ea26b4a65cb5b71be24e5
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-14 21:53:24 +0300

    Mapping classes of the entities were created. Context class created and adjusments in the programcs and appsettings were done for database connection.

---
+++ MovieStore/Models/DataAccess/Mappings/DirectorMapping.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieStore.Models.Entities;
+
+namespace MovieStore.Models.DataAccess.Mappings
+{
+    public class DirectorMapping : IEntityTypeConfiguration<Director>
+    {
+        public void Configure(EntityTypeBuilder<Director> builder)
+        {
+            builder.ToTable("Directors");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(1, 1)
+                .HasColumnOrder(1);
+
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(20)
+                .IsUnicode(true)
+                .HasColumnOrder(2);
+            
+            
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(true)
+                .HasColumnOrder(3);
+
+            builder.Property(x => x.BirthDate)
+                .HasColumnType("date") 
+                .HasColumnOrder(4);
+        }
+    }
+}
--- MovieStore/Program.cs
+++ MovieStore/Program.cs
@@ -1,7 +1,13 @@
+using MovieStore.Models.DataAccess;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MovieDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
