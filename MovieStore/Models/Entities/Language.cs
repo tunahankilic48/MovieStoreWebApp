f0c0e10cfc85f8c1e4ccb5509b4aa0429974ad00
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-14 20:16:01 +0300

    Movie,  Category, Director, Starring and Language entities were added in the Entity folder that is in the Model folder.

---
+++ MovieStore/Models/Entities/Language.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieStore.Models.Entities
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Property
+        public List<Movie> OriginalLanguageOfMovies { get; set; }
+        public List<Movie> SubtitleLanguageOfMovies { get; set; }
+    }
+}
---
+++ MovieStore/Models/Entities/Movie.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace MovieStore.Models.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int CategoryId { get; set; }
+        public int DirectorId { get; set; }
+        public decimal Price { get; set; }
+        public int RunningTimeMin { get; set; }
+        public int LanguageId { get; set; }
+
+
+
+
+        // Navigaton Property
+        public Category Category { get; set; }
+        public Director Director { get; set; }
+        public List<Starring> Starrings { get; set; }
+        public Language Language { get; set; }
+        public List<Language> SubtitleLanguages { get; set; }
+
+
+    }
+}
