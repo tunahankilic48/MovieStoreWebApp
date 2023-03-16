1bc1ce51652d2531161635e0ae4b43e2abcefdfe
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-16 13:11:08 +0300

    IDirector Repository and Director Repository were created.

---
+++ MovieStore/Repository/Abstract/IDirectorRepository.cs
@@ -0,0 +1,8 @@
+﻿using MovieStore.Models.Entities;
+
+namespace MovieStore.Repository.Abstract
+{
+    public interface IDirectorRepository : IBaseRepository<Director>
+    {
+    }
+}
---
+++ MovieStore/Repository/Concrete/DirectorRepository.cs
@@ -0,0 +1,54 @@
+﻿using MovieStore.Models.DataAccess;
+using MovieStore.Models.Entities;
+using MovieStore.Repository.Abstract;
+using System.Linq.Expressions;
+
+namespace MovieStore.Repository.Concrete
+{
+    public class DirectorRepository : IDirectorRepository
+    {
+        private readonly IMovieDbContext _context;
+        public DirectorRepository(IMovieDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Director entity)
+        {
+            _context.Directors.Add(entity);
+            return Save() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            _context.Directors.Remove(GetById(id));
+            return Save() > 0;
+        }
+
+        public ICollection<Director> GetAll()
+        {
+            return _context.Directors.ToList();
+        }
+
+        public Director GetById(int id)
+        {
+            return _context.Directors.Find(id);
+
+        }
+
+        public ICollection<Director> GetDefault(Expression<Func<Director, bool>> exp)
+        {
+            return _context.Directors.Where(exp).ToList();
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public bool Update(Director entity)
+        {
+            _context.Directors.Update(entity);
+            return Save() > 0;
+        }
+    }
+}
